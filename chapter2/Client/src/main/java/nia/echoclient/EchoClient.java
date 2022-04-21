f529c53bb768cd7f4e9f393893a324c25f0fb138
tony-coder <1606771552@qq.com>
2022-04-21 23:19:05 +0800

    创建 echo客户端

---
+++ chapter2/Client/src/main/java/nia/echoclient/EchoClient.java
@@ -0,0 +1,13 @@
+package nia.echoclient;
+
+public class EchoClient {
+    private final String host;
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    
+}
