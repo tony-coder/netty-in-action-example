56fbb5076f837f5e90244be7424d0ee9aa6d364e
tony-coder <1606771552@qq.com>
2022-04-23 23:29:56 +0800

    添加注释

--- chapter2/Server/src/main/java/nia/echoserve/EchoServer.java
+++ chapter2/Server/src/main/java/nia/echoserve/EchoServer.java
@@ -33,19 +33,25 @@ public class EchoServer {
         // 创建EventLoopGroup
         EventLoopGroup group = new NioEventLoopGroup();
         try {
+            // 创建ServerBootstrap
             ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap.group(group)
+                    // 指定所使用的 NIO 传输 Channel
                     .channel(NioServerSocketChannel.class)
+                    // 使用指定的端口设置套接字地址
                     .localAddress(new InetSocketAddress(port))
+                    // 添加一个EchoServerHandler 到子Channel的 ChannelPipeline
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
-                            socketChannel.pipeline().addLast(serverHandler);
+                            socketChannel.pipeline().addLast(serverHandler);  // EchoServerHandler 被标注为@Shareable，所以我们可以总是使用同样的实例
                         }
                     });
+            // 异步地绑定服务器；调用 sync()方法阻塞等待直到绑定完成
             ChannelFuture f = bootstrap.bind().sync();
             System.out.println(EchoServer.class.getName() +
                     " started and listening for connections on " + f.channel().localAddress());
+            // 获取 Channel 的CloseFuture，并且阻塞当前线程直到它完成
             f.channel().closeFuture().sync();
         }finally {
             group.shutdownGracefully().sync();
