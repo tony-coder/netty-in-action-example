76c85b539ccedf8c1df4e62a0f18b0a47d45b936
tony-coder <1606771552@qq.com>
2022-04-23 23:32:47 +0800

    echo客户端

--- chapter2/Client/src/main/java/nia/echoclient/EchoClient.java
+++ chapter2/Client/src/main/java/nia/echoclient/EchoClient.java
@@ -1,5 +1,15 @@
 package nia.echoclient;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
 public class EchoClient {
     private final String host;
     private final int port;
@@ -9,5 +19,37 @@ public class EchoClient {
         this.port = port;
     }
 
-    
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: " + EchoClient.class.getSimpleName() +
+                    " <host> <port>"
+            );
+            return;
+        }
+
+        final String host = args[0];
+        final int port = Integer.parseInt(args[1]);
+        new EchoClient(host, port).start();
+    }
+
+    public void start() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture f = bootstrap.connect().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
 }
